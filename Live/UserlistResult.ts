e645f305d62c9430ef8aebc58b100e1ace2e4310
ilovecherries <cherry@epochal.quest>
2022-05-18 15:40:09 -0400

    improved userlistresult

--- Live/UserlistResult.ts
+++ Live/UserlistResult.ts
@@ -1,8 +1,10 @@
+import { Content, User } from "../Views";
 import { LiveData } from "./LiveData";
 
-export interface UserlistResult extends LiveData {
+export interface UserlistResult {
 	/**
 	 * { Content ID: { User ID: Status } }
 	 */
 	statuses: Record<number, Record<number, string>>;
+	objects: { content: Partial<Content>[], user: User[] };
 }
