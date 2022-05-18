22643082f4ebeb29e839785504646ea8b2fcef99
ilovecherries <cherry@epochal.quest>
2022-05-18 14:27:47 -0400

    used the wrong enum for livedata

--- Live/LiveData.ts
+++ Live/LiveData.ts
@@ -1,8 +1,9 @@
 import { LiveEvent, LiveEventType } from "./LiveEvent";
+import { WebsocketResponseType } from "./WebSocketResponse";
 
 export interface LiveData {
 	optimized?: boolean;
 	lastId?: number;
 	events?: LiveEvent[];
-	objects: Record<LiveEventType, Record<string, Array<object>>>;
+	objects: Record<WebsocketResponseType, Record<string, Array<object>>>;
 }
