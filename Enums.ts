917517522461b18575393cdf57d6fe76dba8bb5e
ilovecherries <cherry@epochal.quest>
2022-05-20 12:56:23 -0400

    added userpage file type

--- Enums.ts
+++ Enums.ts
@@ -1,6 +1,12 @@
 export enum BanType { none = 0, public = 1, private = 2 }
 
-export enum InternalContentType { none = 0, page = 1, module = 2, file = 3 }
+export enum InternalContentType {
+	none = 0,
+	page = 1,
+	module = 2,
+	file = 3,
+	userpage = 4,
+}
 
 export enum UserAction { create = 1, read = 2, update = 4, delete = 8 }
 
